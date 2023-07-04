2e1f9099d506e0956deaefefabdb302cf71ca8cc
Maijs Garais <mai-soup@mailfence.com>
2023-07-04 10:15:41 +0100

    initial design and data fetching

---
+++ src/Cards.tsx
@@ -0,0 +1,33 @@
+interface CardProps {
+    topic: string,
+    imageUrl: string,
+    title: string,
+    authorName: string,
+    authorUrl: string,
+    createdOn: string,
+    entryType: string,
+    imageAlt: string,
+}
+
+const Card: React.FC<CardProps> = ({ topic, imageUrl, title, authorName = "Author", authorUrl, createdOn, entryType = "Entry Type", imageAlt }) => {
+    const date = new Date(createdOn);
+    createdOn = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
+    return (
+        <div className="p-card--highlighted" style={{ boxShadow: "inset 0 3px 0 0 orchid, 0 0 6px rgba(0,0,0,0.2)" }}>
+            <p className="p-text--small-caps">{topic}</p>
+            <hr className="is-muted" />
+            <div className="p-card__content">
+                <img className="p-card__image" height="185" width="330" src={imageUrl} alt={imageAlt} />
+                <h4>
+                    <a href="#">{title}</a>
+                </h4>
+                <em><p className="u-left">By <a href={authorUrl}>{authorName}</a> on {createdOn}</p></em>
+            </div>
+            <hr className="is-muted" />
+            <small>{entryType}</small>
+        </div>
+    );
+}
+
+export default Card;
